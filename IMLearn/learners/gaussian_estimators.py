c1e63c30cdab11a8618f0fe4dfcfaa5b7aa6cb36
Eyal Miller <eyal.miller202@gmail.com>
2022-03-14 23:48:37 +0200

    Finished exercise 1

--- IMLearn/learners/gaussian_estimators.py
+++ IMLearn/learners/gaussian_estimators.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
 import numpy as np
 from numpy.linalg import inv, det, slogdet
+import matplotlib.pyplot as plt
 
 
 class UnivariateGaussian:
     """
     Class for univariate Gaussian Distribution Estimator
     """
-    def __init__(self, biased_var: bool = False) -> UnivariateGaussian:
+
+    def __init__(self, biased_var: bool = False):
         """
         Estimator for univariate Gaussian mean and variance parameters
 
@@ -18,28 +20,29 @@ class UnivariateGaussian:
 
         Attributes
         ----------
-        fitted_ : bool
+        fitted_ - bool
             Initialized as false indicating current estimator instance has not been fitted.
             To be set as True in `UnivariateGaussian.fit` function.
 
-        mu_: float
+        mu_ - float
             Estimated expectation initialized as None. To be set in `UnivariateGaussian.fit`
             function.
 
-        var_: float
+        var_ - float
             Estimated variance initialized as None. To be set in `UnivariateGaussian.fit`
             function.
         """
         self.biased_ = biased_var
+        self.x_values = None
         self.fitted_, self.mu_, self.var_ = False, None, None
 
-    def fit(self, X: np.ndarray) -> UnivariateGaussian:
+    def fit(self, x: np.ndarray) -> UnivariateGaussian:
         """
         Estimate Gaussian expectation and variance from given samples
 
         Parameters
         ----------
-        X: ndarray of shape (n_samples, )
+        x: ndarray of shape (n_samples, )
             Training data
 
         Returns
@@ -51,18 +54,19 @@ class UnivariateGaussian:
         Sets `self.mu_`, `self.var_` attributes according to calculated estimation (where
         estimator is either biased or unbiased). Then sets `self.fitted_` attribute to `True`
         """
-        raise NotImplementedError()
 
+        self.mu_ = x.mean()
+        self.var_ = x.var()
         self.fitted_ = True
         return self
 
-    def pdf(self, X: np.ndarray) -> np.ndarray:
+    def pdf(self, x: np.ndarray) -> np.ndarray:
         """
         Calculate PDF of observations under Gaussian model with fitted estimators
 
         Parameters
         ----------
-        X: ndarray of shape (n_samples, )
+        x: ndarray of shape (n_samples, )
             Samples to calculate PDF for
 
         Returns
@@ -76,10 +80,11 @@ class UnivariateGaussian:
         """
         if not self.fitted_:
             raise ValueError("Estimator must first be fitted before calling `pdf` function")
-        raise NotImplementedError()
+        pdf = 1 / np.sqrt(2 * np.pi * self.var_) * np.exp(-1 * np.power(x - self.mu_, 2) / (2 * self.var_))
+        return pdf
 
     @staticmethod
-    def log_likelihood(mu: float, sigma: float, X: np.ndarray) -> float:
+    def log_likelihood(mu: float, sigma: float, x: np.ndarray) -> float:
         """
         Calculate the log-likelihood of the data under a specified Gaussian model
 
@@ -89,7 +94,7 @@ class UnivariateGaussian:
             Expectation of Gaussian
         sigma : float
             Variance of Gaussian
-        X : ndarray of shape (n_samples, )
+        x : ndarray of shape (n_samples, )
             Samples to calculate log-likelihood with
 
         Returns
@@ -97,41 +102,47 @@ class UnivariateGaussian:
         log_likelihood: float
             log-likelihood calculated
         """
-        raise NotImplementedError()
+        sum_helper = 0
+        for sample in x:
+            sum_helper += (sample - mu) * (sample - mu)
+        log_likelihood = np.log(1 / np.power(2 * np.pi * sigma * sigma, len(x) / 2)) * np.exp(
+            -1 / (2 * sigma * sigma) * sum_helper)
+        return log_likelihood
 
 
 class MultivariateGaussian:
     """
     Class for multivariate Gaussian Distribution Estimator
     """
+
     def __init__(self):
         """
         Initialize an instance of multivariate Gaussian estimator
 
         Attributes
         ----------
-        fitted_ : bool
+        fitted_ - bool
             Initialized as false indicating current estimator instance has not been fitted.
             To be set as True in `MultivariateGaussian.fit` function.
 
-        mu_: ndarray of shape (n_features,)
+        mu_ - ndarray of shape (n_features,)
             Estimated expectation initialized as None. To be set in `MultivariateGaussian.fit`
             function.
 
-        cov_: ndarray of shape (n_features, n_features)
+        cov_ - ndarray of shape (n_features, n_features)
             Estimated covariance initialized as None. To be set in `MultivariateGaussian.fit`
             function.
         """
         self.mu_, self.cov_ = None, None
         self.fitted_ = False
 
-    def fit(self, X: np.ndarray) -> MultivariateGaussian:
+    def fit(self, x: np.ndarray) -> MultivariateGaussian:
         """
         Estimate Gaussian expectation and covariance from given samples
 
         Parameters
         ----------
-        X: ndarray of shape (n_samples, n_features)
+        x: ndarray of shape (n_samples, n_features)
             Training data
 
         Returns
@@ -143,18 +154,20 @@ class MultivariateGaussian:
         Sets `self.mu_`, `self.cov_` attributes according to calculated estimation.
         Then sets `self.fitted_` attribute to `True`
         """
-        raise NotImplementedError()
-
+        self.mu_ = np.mean(x, axis=0)
+        self.cov_ = np.cov(x, rowvar=False)
+        # print(self.mu_)
+        # print(self.cov_)
         self.fitted_ = True
         return self
 
-    def pdf(self, X: np.ndarray):
+    def pdf(self, x: np.ndarray):
         """
         Calculate PDF of observations under Gaussian model with fitted estimators
 
         Parameters
         ----------
-        X: ndarray of shape (n_samples, n_features)
+        x: ndarray of shape (n_samples, n_features)
             Samples to calculate PDF for
 
         Returns
@@ -168,10 +181,15 @@ class MultivariateGaussian:
         """
         if not self.fitted_:
             raise ValueError("Estimator must first be fitted before calling `pdf` function")
-        raise NotImplementedError()
+        my_pdf = np.array([])
+        for sample in x:
+            my_pdf = np.append(my_pdf, 1 / np.sqrt(np.power(2 * np.pi, len(x[0])) * np.exp(det(self.cov_)))
+                               * np.exp(
+                -1 / 2 * np.matmul((sample - self.mu_), np.matmul(inv(self.cov_), np.transpose(sample - self.mu_)))))
+        return my_pdf
 
     @staticmethod
-    def log_likelihood(mu: np.ndarray, cov: np.ndarray, X: np.ndarray) -> float:
+    def log_likelihood(mu: np.ndarray, cov: np.ndarray, x: np.ndarray) -> float:
         """
         Calculate the log-likelihood of the data under a specified Gaussian model
 
@@ -181,7 +199,7 @@ class MultivariateGaussian:
             Expectation of Gaussian
         cov : ndarray of shape (n_features, n_features)
             covariance matrix of Gaussian
-        X : ndarray of shape (n_samples, n_features)
+        x : ndarray of shape (n_samples, n_features)
             Samples to calculate log-likelihood with
 
         Returns
@@ -189,4 +207,9 @@ class MultivariateGaussian:
         log_likelihood: float
             log-likelihood calculated over all input data and under given parameters of Gaussian
         """
-        raise NotImplementedError()
+        m = len(x)
+        # the trace is here because I wanted the sum over the same X samples, which means that I need only the values
+        # in the main diagonal
+        sum_helper = np.matmul((x - mu), np.matmul(inv(cov), np.transpose(x - mu))).trace()
+        log_likelihood = -len(x[0]) * m / 2 * np.log(2 * np.pi) - m / 2 * slogdet(cov)[1] - 1 / 2 * sum_helper
+        return log_likelihood
--- exercises/fit_gaussian_estimators.py
+++ exercises/fit_gaussian_estimators.py
@@ -1,33 +1,150 @@
 from IMLearn.learners import UnivariateGaussian, MultivariateGaussian
+import matplotlib.pyplot as plt
 import numpy as np
-import plotly.graph_objects as go
-import plotly.io as pio
-pio.templates.default = "simple_white"
+
+# Q2:
+''''''
+
+# Q3:
+'''pdf = my_gauss.pdf(dataset)
+# plt.plot(x, dataset_plot)
+plt.scatter(dataset, np.zeros(NUM))
+plt.scatter(dataset, pdf, edgecolors='red')
+
+plt.show()'''
+
+# Q4:
+'''multi_gauss = MultivariateGaussian()
+multi_mu = [0, 0, 4, 0]
+cov = np.array([[1, 0.2, 0, 0.5], [0.2, 2, 0, 0], [0, 0, 1, 0], [0.5, 0, 0, 1]])
+multi_dataset = np.random.multivariate_normal(multi_mu, cov, NUM)
+multi_gauss.fit(multi_dataset)
+
+mu_range_num = 200
+f1 = np.linspace(-10, 10, mu_range_num)
+f3 = np.linspace(-10, 10, mu_range_num)
+all_mu = np.zeros((mu_range_num, mu_range_num, 4))
+results = np.zeros((mu_range_num, mu_range_num))
+for i in range(mu_range_num):
+    all_mu[i, :, 0] = f1
+    all_mu[:, i, 2] = f3
+for i in range(mu_range_num):
+    for j in range(mu_range_num):
+        results[i][j] = MultivariateGaussian.log_likelihood(all_mu[i][j], cov, multi_dataset)
+        print(i, j)
+
+fig, my_plot = plt.subplots()
+image = my_plot.imshow(results, extent=[-10, 10, -10, 10])
+
+# Show all ticks and label them with the respective list entries
+plt.show()
+max_value = np.amax(results)
+max_index = np.where(results == max_value)
+print(max_index[0], max_index[1])
+print(max_value)'''
 
 
 def test_univariate_gaussian():
     # Question 1 - Draw samples and print fitted model
-    raise NotImplementedError()
+    print("Q1:")
+    my_gauss = UnivariateGaussian()
+    dataset = np.random.normal(MU, VAR, NUM)
+    # x = np.linspace(5, 15, NUM)
+    my_gauss.fit(dataset)
+    print("(" + str(my_gauss.mu_) + ", " + str(my_gauss.var_) + ")")
+
+    print("---------------------------------------------------------")
 
     # Question 2 - Empirically showing sample mean is consistent
-    raise NotImplementedError()
+    print("Q2:")
+    sample_count = np.linspace(10, 1000, num=100).astype(int)
+    mean_estimators = np.array([])
+    for n_samples in sample_count:
+        new_gauss = UnivariateGaussian()
+        new_gauss.fit(np.random.choice(dataset, n_samples))
+        mean_estimators = np.append(mean_estimators, new_gauss.mu_)
+
+    plt.plot(sample_count, np.abs(mean_estimators - 10))
+    plt.title("Error of estimator as a function of the number of samples")
+    plt.ylabel("Abs(EstimatedMean - RealMean)")
+    plt.xlabel("number of samples")
+    plt.show()
+
+    print("---------------------------------------------------------")
 
     # Question 3 - Plotting Empirical PDF of fitted model
-    raise NotImplementedError()
+    print("Q3:")
+    pdf = my_gauss.pdf(dataset)
+    plt.title("the fitted gaussian and the empirical pdf")
+    plt.scatter(dataset, np.zeros(NUM))
+    plt.scatter(dataset, pdf, color='red')
+    plt.legend(["fitted gaussian", "samples"])
+    plt.xlabel("sample value")
+    plt.ylabel("pdf")
+    plt.show()
 
 
 def test_multivariate_gaussian():
     # Question 4 - Draw samples and print fitted model
-    raise NotImplementedError()
+    print("Q4:")
+    multi_gauss = MultivariateGaussian()
+    multi_mu = [0, 0, 4, 0]
+    cov = np.array([[1, 0.2, 0, 0.5], [0.2, 2, 0, 0], [0, 0, 1, 0], [0.5, 0, 0, 1]])
+    multi_dataset = np.random.multivariate_normal(multi_mu, cov, NUM)
+    multi_gauss.fit(multi_dataset)
+    print(multi_gauss.mu_)
+    print(multi_gauss.cov_)
+
+    print("---------------------------------------------------------")
 
     # Question 5 - Likelihood evaluation
-    raise NotImplementedError()
+    print("Q5:")
+    f1 = np.linspace(-10, 10, MU_SAMPLES)
+    f3 = np.linspace(-10, 10, MU_SAMPLES)
+    f3 = np.flip(f3)  # so that the graph will start at (-10, -10) and end in (10, 10)
+    all_mu = np.zeros((MU_SAMPLES, MU_SAMPLES, 4))
+    results = np.zeros((MU_SAMPLES, MU_SAMPLES))
+    for i in range(MU_SAMPLES):
+        # keep in mind - I intentionally swapped f1 and f3 so that they will be on the correct
+        # axis according to the question
+        all_mu[i, :, 0] = f1
+        all_mu[:, i, 2] = f3
+    for i in range(MU_SAMPLES):
+        for j in range(MU_SAMPLES):
+            results[i][j] = MultivariateGaussian.log_likelihood(all_mu[i][j], cov, multi_dataset)
+    _, my_plot = plt.subplots()
+    my_plot.imshow(results, extent=[-10, 10, -10, 10])
+    plt.title("heatmap of log-likelihood as a function of f1, f3")
+    plt.xlabel("f1")
+    plt.ylabel("f3")
+    plt.show()
+    print("I can learn from the graph that the best values are near the original values\n"
+          "we used to fit the model - 0 for f1 and 4 for f3")
+    print("---------------------------------------------------------")
 
     # Question 6 - Maximum likelihood
-    raise NotImplementedError()
+    print("Q6:")
+    max_value = np.amax(results)
+    print("Max Value:", max_value.round(3))
+    max_index_f1 = np.where(results == max_value)[1][0]
+    max_index_f3 = np.where(results == max_value)[0][0]
+    print("Could be find in indices:", max_index_f1, max_index_f3)
+    print("It means that the best model is when:")
+
+    # I used the linspace and adjusted the index so that I could find the original value,
+    # because the center is 100 and the length is 10:
+    best_f1 = (max_index_f1 - 100) / 10
+    best_f3 = (max_index_f3 - 100) / 10
+    print("f1 =", best_f1)
+    print("f3 =", best_f3)
 
 
 if __name__ == '__main__':
+    FEATURES = 4
+    NUM = 250
+    MU = 10
+    VAR = 1
+    MU_SAMPLES = 200
     np.random.seed(0)
     test_univariate_gaussian()
     test_multivariate_gaussian()
